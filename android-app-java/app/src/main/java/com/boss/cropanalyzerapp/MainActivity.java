c524b36b9a412be34d7a69ba06810e5dda225e2b
bavanya <44469603+bavanya@users.noreply.github.com>
2020-04-28 19:38:40 +0530

    change1
    created function and declared changes

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //declaring variables for all the views
+    Button button;
+    ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
     }
+    
+    private void bindView(){
+        button = findViewById(R.id.button);
+        imageview=findViewById(R.id.image_view);
 }
