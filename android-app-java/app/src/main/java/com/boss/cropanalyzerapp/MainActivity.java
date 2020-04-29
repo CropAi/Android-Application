a21ee5155b2ceaa601bd46e5ff2afecef1f780a0
Tanishq Saluja <tanishqsaluja18@gmail.com>
2020-04-30 00:37:57 +0530

    ui change

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
@@ -5,24 +5,23 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private static final int IMAGE_PICK_CODE = 100;
     private static final int PERMISSION_CODE = 101;
+    private static final int RESET_VIEWS = 102;
+    String TAG = "MainActivity";
+    Boolean userSelectedImage = false;
 
     Button uploadButton;
     ImageView imageView;
@@ -31,21 +30,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindView();
+        bindViews();
+        clickListeners();
+    }
 
+    private void clickListeners() {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //check runtime permission
+            public void onClick(View view) {
+                Log.e("TAG", String.valueOf(userSelectedImage));
+                if (userSelectedImage) {
+                    // Make Network Request
 
+
+                    // start new activity and send JSON to this activity
+
+                    // use start activity for result eg : startActivityForResult(intent, RESET_VIEWS)
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("TAG", String.valueOf(userSelectedImage));
+                //check runtime permission
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
                         //permission not granted, requesting permission
-
                         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-
                         //show popup for runtime permission
-
                         requestPermissions(permissions, PERMISSION_CODE);
                     } else {
                         //permission already granted
@@ -59,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void bindView() {
+    public void bindViews() {
         uploadButton = findViewById(R.id.buttonUpload);
         imageView = findViewById(R.id.imageView);
     }
@@ -92,9 +110,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            String TAG = "Image location";
-            Log.e(TAG, data.getData().toString());
-            imageView.setImageURI(data.getData());
+            switch (requestCode) {
+                case IMAGE_PICK_CODE: {
+                    Log.e(TAG, data.getData().toString());
+                    imageView.setImageURI(data.getData());
+                    userSelectedImage = true;
+                }
+                case RESET_VIEWS: {
+                    // reset the imageView
+                    userSelectedImage = false;
+                    // imageView.setBackground(R.drawable.ic_leaf);
+                }
+            }
+
         }
     }
 }
