07f94ae73b8e54618e652ac5b4d300aa2bb8678a
Shwetagurnani <Shwetaagra@gmail.com>
2020-04-29 18:37:15 +0530

    Added functionality to upload button

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
@@ -16,10 +16,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindView();
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check runtime permission
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        //permission not granted, requesting permission
+
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+                        //show popup for runtime permission
+
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        //permission already granted
+                        uploadImage();
+                    }
+                } else {
+                    //system os is less than marshmallow
+                    uploadImage();
+                }
+            }
+        });
     }
 
     public void bindView() {
         uploadButton = findViewById(R.id.buttonUpload);
         imageView = findViewById(R.id.imageView);
+    }
+
+    private void uploadImage()() {
+        //intent to upload image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    //handling request of runtime permission
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    uploadImage();
+                }
+                else
+                {
+                    //permission denied
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    //handling result of pick image
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE)
+        {
+            String TAG = "Image location";
+            Log.e(TAG, String.valueOf(data.getData()));
+            imageView.setImageURI(data.getData());
+        }
     }
 }
