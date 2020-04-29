df7e75c7a22718e701f3eec6d6764031797bdec5
abhishekgupta368 <abhigupta9087@gmail.com>
2020-04-29 16:43:36 +0530

    added imageView and bindView

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.boss.cropanalyzerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button upload_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        upload_image = (Button)findViewById(R.id.buttonUpload);
+        upload_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bindView();
+            }
+        });
+    }
+    public void bindView(){
+
     }
 }
