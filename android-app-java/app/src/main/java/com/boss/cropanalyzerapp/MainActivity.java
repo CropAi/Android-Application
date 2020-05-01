570eed79fb781202e6e1a85978fe1bab5aedd3a9
Tanishq Saluja <tanishqsaluja18@gmail.com>
2020-05-01 23:24:41 +0530

    added networking code

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
@@ -5,44 +5,82 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
-    private static boolean IMAGE_CHECK = false;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
     private static final int IMAGE_PICK_CODE = 100;
     private static final int PERMISSION_CODE = 101;
+    private static final int RESET_VIEWS = 102;
+    private static final String IP = "http://192.168.1.4:3000/file_upload";
+    private static final String CROP_LEAF_URL = "https://crop-leaf.herokuapp.com/file_upload";
+    String image_path = "";
+    String TAG = "MainActivity";
+    Boolean userSelectedImage = false;
 
-    Button analyzeButton;
+    ProgressDialog progressDialog;
+    Button uploadButton;
     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindView();
+        bindViews();
+        clickListeners();
+    }
+
+    private void clickListeners() {
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, String.valueOf(userSelectedImage));
+                if (userSelectedImage) {
+                    // Make Network Request
+                    getLeafCategory();
+                } else {
+                    Toast.makeText(MainActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.e("TAG", String.valueOf(userSelectedImage));
                 //check runtime permission
-
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
                         //permission not granted, requesting permission
-
                         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-
                         //show popup for runtime permission
-
                         requestPermissions(permissions, PERMISSION_CODE);
                     } else {
                         //permission already granted
@@ -54,27 +92,111 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
-        analyzeButton.setOnClickListener(new View.OnClickListener() {
+    private void getLeafCategory() {
+
+        File file = new File(image_path);
+        final String file_path = file.getAbsolutePath();
+        // extension can be .png/.PNG/.jpg/.JPG/.jpeg/.JPEG
+        String extension = file_path.substring(file_path.length() - 3);
+        Log.e(TAG, extension);
+        if (extension.equals("png") || extension.equals("PNG")) {
+            makeRequest("png");
+        } else if (extension.equals("jpg") || extension.equals("JPG")) {
+            makeRequest("jpg");
+        } else if (extension.equals("peg") || extension.equals("PEG")) {
+            makeRequest("jpeg");
+        } else {
+            Toast.makeText(MainActivity.this, "Incorrect File Format", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void makeRequest(String extension) {
+
+        // Disable the button for multiple requests
+        uploadButton.setClickable(false);
+        uploadButton.setAlpha(.5f);
+        imageView.setClickable(false);
+
+        // make a loader on the main thread
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Analyzing");
+        progressDialog.setProgress(30);//initially progress is 30 seconds
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        new CountDownTimer(30000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                progressDialog.setMessage(millisUntilFinished / 1000 + " SECONDS REMAINING");
+            }
+
+            public void onFinish() {
+                progressDialog.setMessage("ALMOST THERE");
+            }
+        }.start();
+
+        File file = new File(image_path);
+        final String file_path = file.getAbsolutePath();
+
+        RequestBody file_body = RequestBody.create(MediaType.parse("image/" + extension), file);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(1, TimeUnit.MINUTES) // connect timeout
+                .writeTimeout(1, TimeUnit.MINUTES) // write timeout
+                .readTimeout(1, TimeUnit.MINUTES); // read timeout
+
+        OkHttpClient client = builder.build();
+
+        RequestBody request_body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", file_path.substring(file_path.lastIndexOf("/") + 1), file_body)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(CROP_LEAF_URL)
+                .post(request_body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
             @Override
-            public void onClick(View v) {
-                if (!IMAGE_CHECK) {
-                    Toast.makeText(getApplicationContext(), "No File Selected", Toast.LENGTH_SHORT).show();
-                } else {
+            public void onFailure(Call call, final IOException e) {
+                // Hide Progress dialog
+                progressDialog.dismiss();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_LONG).show();
+                        resetViews();
+                    }
+                });
+                Log.e(TAG, "ERROR:" + e.getMessage());
+            }
 
-                    // Make network request here and launch result activity on getting response
-                    // pass the response in intent to the result activity
-
-
-                    Intent intent =  new Intent(MainActivity.this, ResultActivity.class);
-                    startActivity(intent);
-                }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // Hide Progress dialog
+                progressDialog.setMessage("DONE");
+                progressDialog.dismiss();
+                String jsonResponse = response.body().string();
+                Log.e(TAG, "RESPONSE:" + jsonResponse);
+                launchResultActivity(jsonResponse);
             }
         });
     }
 
-    public void bindView() {
-        analyzeButton = findViewById(R.id.buttonAnalyze);
+    // Launch result activity
+    private void launchResultActivity(String jsonResponse) {
+        // start new activity and send JSON to this activity
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+        intent.putExtra("response", jsonResponse);
+        // we will reset the imageView in OnActivityResult
+        startActivityForResult(intent, RESET_VIEWS);
+    }
+
+    public void bindViews() {
+        uploadButton = findViewById(R.id.buttonAnalyze);
         imageView = findViewById(R.id.imageView);
     }
 
@@ -82,11 +204,9 @@ public class MainActivity extends AppCompatActivity {
         //intent to upload image
         Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, IMAGE_PICK_CODE);
-        IMAGE_CHECK = true;
     }
 
     //handling request of runtime permission
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -102,14 +222,51 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //handling result of pick image
+    // handling result of pick image and going back from result activity to main activity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            String TAG = "Image location";
-            Log.e(TAG, data.getData().toString());
-            imageView.setImageURI(data.getData());
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case IMAGE_PICK_CODE: {
+                    Log.e(TAG, data.getData().getPath().split(":")[1]);
+                    imageView.setImageURI(data.getData());
+                    userSelectedImage = true;
+
+                    Uri selectedImage = data.getData();
+                    String[] filePath = {MediaStore.Images.Media.DATA};
+                    Log.e(TAG, filePath.toString());
+                    // content resolver helps us to get the access of different content providers
+                    Cursor cursor = this.getContentResolver().query(selectedImage, filePath, null, null, null);
+                    if (cursor != null) {
+                        cursor.moveToFirst();
+                        int columnIndexedValue = cursor.getColumnIndex(filePath[0]);
+                        String picturePath = cursor.getString(columnIndexedValue);
+                        cursor.close();
+                        image_path = picturePath;
+                        Log.e(TAG, "PIC PATH :" + picturePath);
+                        Glide.with(this).load(picturePath).into(imageView);
+                    }
+                    break;
+                }
+                case RESET_VIEWS: {
+                    // reset the boolean variable and imageView
+                    Log.e(TAG, "Back Inside Main Activity");
+                    resetViews();
+                    break;
+                }
+            }
+
         }
+    }
+
+    private void resetViews() {
+        userSelectedImage = false;
+        imageView.setImageResource(R.drawable.ic_leaf);
+        // reset the upload button
+        uploadButton.setClickable(true);
+        uploadButton.setAlpha(1f);
+        // reset the imageView
+        imageView.setClickable(true);
     }
 }
