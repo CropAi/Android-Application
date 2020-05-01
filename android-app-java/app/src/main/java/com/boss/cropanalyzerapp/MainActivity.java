bfdd34bf5a6a1224e0e6cf6f2ff27ae798c38174
Tanishq Saluja <tanishqsaluja18@gmail.com>
2020-05-01 15:40:45 +0530

    added log

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/MainActivity.java
@@ -2,34 +2,29 @@ package com.boss.cropanalyzerapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity {
 
     private static boolean IMAGE_CHECK = false;
     private static final int IMAGE_PICK_CODE = 100;
     private static final int PERMISSION_CODE = 101;
+    String TAG = "MainActivity";
+    String image_path = "";
 
     Button analyzeButton;
     ImageView imageView;
@@ -109,33 +104,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public String getUrlPath(Uri path){
-        String image_path="";
+    //handling result of pick image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            Log.e(TAG, data.getData().toString());
+            //  We will pass the Uri , Get Path of the image
+            imageView.setImageURI(data.getData());
+            image_path = getUrlPath(data.getData());
+        }
+    }
+
+    public String getUrlPath(Uri path) {
+        String picturePath = "";
         Uri selectedImage = path;
         String[] filePath = {MediaStore.Images.Media.DATA};
         Cursor cursor = this.getContentResolver().query(selectedImage, filePath, null, null, null);
         if (cursor != null) {
             cursor.moveToFirst();
             int columnIndexedValue = cursor.getColumnIndex(filePath[0]);
-            String picturePath = cursor.getString(columnIndexedValue);
+            picturePath = cursor.getString(columnIndexedValue);
             cursor.close();
-            image_path = picturePath;
         }
-        return image_path;
+        Log.e(TAG, picturePath);
+        return picturePath;
     }
-    
-    //handling result of pick image
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            String TAG = "Image location";
-            Log.e(TAG, data.getData().toString());
 
-            //  We will pass the Uri , Get Path of the image
-            Log.e(TAG,getUrlPath(data.getData()));
-
-            imageView.setImageURI(data.getData());
-        }
-    }
 }
