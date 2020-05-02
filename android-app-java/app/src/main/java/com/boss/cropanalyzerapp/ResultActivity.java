2de14e47a439cf4de2643f5aea4fe94cbf77a158
Tanishq Saluja <tanishqsaluja18@gmail.com>
2020-05-02 07:46:22 +0530

    minor reverts

--- android-app-java/app/src/main/java/com/boss/cropanalyzerapp/ResultActivity.java
+++ android-app-java/app/src/main/java/com/boss/cropanalyzerapp/ResultActivity.java
@@ -3,7 +3,6 @@ package com.boss.cropanalyzerapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -40,8 +39,8 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     private void parsingData() throws JSONException {
-        String responseData = "{\"category\":\"Pepper Bell Bacterial Spot\",\"Symptoms\":{\"1\":\"Leaf spots that appear on the lower surface of older leaves as small, pimples and on the upper leaf surface as small water-soaked spots are a symptom of bacterial spot.\",\"2\":\"Eventually, the spots develop gray to tan centers with darker borders. Lesions enlarge during warm, humid weather. Leaves may then turn yellow, then brown and drop. Lesions may also develop on stems.\",\"3\":\"Lesions may also develop on stems. Fruits develop small, raised rough spots that do not affect eating quality.\"},\"Treatment\":{\"1\":\"Treat seeds by soaking them for 2 minutes in a 10% chlorine bleach solution (1 part bleach; 9 parts water). Thoroughly rinse seeds and dry them before planting.\",\"2\":\" Mulch plants deeply with a thick organic material like newspaper covered with straw or grass clippings.\",\"3\":\"Spray every 10-14 days with fixed copper (organic fungicide) to slow down the spread of infection.\",\"4\":\"Rotate peppers to a different location if infections are severe and cover the soil with black plastic mulch or black landscape fabric prior to planting. \"},\"Recommended_Product\":{\"1\":\"Neem Oil : https://www.planetnatural.com/product/organic-neem-oil/\",\"2\":\"Garden Dust : https://www.planetnatural.com/product/garden-dust/\"}}\n";
-        // String responseData = getIntent().getStringExtra("response");
+        // String responseData = "{\"category\":\"Pepper Bell Bacterial Spot\",\"Symptoms\":{\"1\":\"Leaf spots that appear on the lower surface of older leaves as small, pimples and on the upper leaf surface as small water-soaked spots are a symptom of bacterial spot.\",\"2\":\"Eventually, the spots develop gray to tan centers with darker borders. Lesions enlarge during warm, humid weather. Leaves may then turn yellow, then brown and drop. Lesions may also develop on stems.\",\"3\":\"Lesions may also develop on stems. Fruits develop small, raised rough spots that do not affect eating quality.\"},\"Treatment\":{\"1\":\"Treat seeds by soaking them for 2 minutes in a 10% chlorine bleach solution (1 part bleach; 9 parts water). Thoroughly rinse seeds and dry them before planting.\",\"2\":\" Mulch plants deeply with a thick organic material like newspaper covered with straw or grass clippings.\",\"3\":\"Spray every 10-14 days with fixed copper (organic fungicide) to slow down the spread of infection.\",\"4\":\"Rotate peppers to a different location if infections are severe and cover the soil with black plastic mulch or black landscape fabric prior to planting. \"},\"Recommended_Product\":{\"1\":\"Neem Oil : https://www.planetnatural.com/product/organic-neem-oil/\",\"2\":\"Garden Dust : https://www.planetnatural.com/product/garden-dust/\"}}\n";
+        String responseData = getIntent().getStringExtra("response");
         // Parse this JSON and inflate views to show the results in a beautiful manner
         JSONObject jsonObject = new JSONObject(responseData);
         // set the title of toolbar
